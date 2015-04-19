804aaf8da922c8e7d21a1bea63a61721f5ae0a60
BoruiGu <alax919@gmail.com>
2015-04-19 10:01:51 -0400

    Add comment in datetime filter

--- src/filter/datetime.js
+++ src/filter/datetime.js
@@ -1,5 +1,6 @@
 ﻿app.filter('datetime', function () {
     return function (input) {
+        /* Covert to user's local time */
         var t = new Date(input);        
         return t.toString();
     };
